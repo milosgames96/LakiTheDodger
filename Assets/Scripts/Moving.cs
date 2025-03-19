5ca6a5c9d6d34ce756a75f7a96627f4cb5202b90
milosgames96 <milos772057@gmail.com>
2025-03-19 15:04:12 +0100

    Animator improved
    Change transitions to make them smoother.

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -5,6 +5,7 @@ public class Moving : MonoBehaviour
 {
     [SerializeField] float movespeed = 4f;
     [SerializeField] float rotationspeed = 100f;
+    [SerializeField] private Animator animator;
     public static bool alive;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,12 +25,22 @@ public class Moving : MonoBehaviour
     {
         if (!alive) return;
 
-        float xValue = 0f;
-        float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
-        transform.Translate(xValue, yValue, zValue);
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("isWalking", true);
 
-        float angle = Input.GetAxis("Horizontal") * Time.deltaTime * rotationspeed;
+            float xValue = 0f;
+            float yValue = 0f;
+            float zValue = Input.GetAxis("Vertical") * Time.deltaTime * movespeed;
+            transform.Translate(xValue, yValue, zValue);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+
+            float angle = Input.GetAxis("Horizontal") * Time.deltaTime * rotationspeed;
         transform.Rotate(0f, angle, 0f);
     }
 }
